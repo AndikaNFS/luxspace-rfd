c6999806abfd2ad505acc717bc7d81d15295a698
Andika Nur Sasmito <56013918+AndikaNFS@users.noreply.github.com>
2023-04-07 19:01:39 +0700

    add folder

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import './App.css';
+import './assets/css/App.css';
 
 function App() {
   return (
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h1 className="text-center w-10">Coba gan</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
